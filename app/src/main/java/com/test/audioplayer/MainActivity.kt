d3d488d66f54275f19b4a7e390fd2793f5b2d4ae
Haftor <haftor.g@gmail.com>
2018-11-05 18:25:39 +0000

    Plays a random song and displays song content.

--- app/src/main/java/com/test/audioplayer/MainActivity.kt
+++ app/src/main/java/com/test/audioplayer/MainActivity.kt
@@ -2,6 +2,8 @@ package com.test.audioplayer
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.graphics.Typeface
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.design.widget.Snackbar
@@ -10,14 +12,19 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import com.mtechviral.mplaylib.MusicFinder
 
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.longToast
+import kotlinx.coroutines.experimental.launch
+import org.jetbrains.anko.*
+import org.jetbrains.anko.custom.async
+import org.jetbrains.anko.sdk25.coroutines.onClick
 import java.io.File
-import java.util.ArrayList
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -65,7 +72,91 @@ class MainActivity : AppCompatActivity() {
 
     private fun createPlayer()
     {
+        var songJob = kotlinx.coroutines.experimental.async {
+            val songFinder = MusicFinder(contentResolver)
+            songFinder.prepare()
+            songFinder.allSongs
+        }
 
+        launch(kotlinx.coroutines.experimental.android.UI) {
+            val songs = songJob.await()
+
+            val playerUI = object:AnkoComponent<MainActivity>
+            {
+                override fun createView(ui: AnkoContext<MainActivity>)= with(ui) {
+
+                    relativeLayout{
+                        backgroundColor = Color.BLACK
+
+                        albumArt = imageView{
+                            scaleType = ImageView.ScaleType.FIT_CENTER
+                        }.lparams(matchParent, matchParent)
+
+                        verticalLayout{
+                            backgroundColor = Color.parseColor("#99000000")
+                            songTitle = textView{
+                                textColor = Color.WHITE
+                                typeface = Typeface.DEFAULT_BOLD
+                                textSize = 18f
+                            }
+
+                            songArtist = textView{
+                                textColor = Color.WHITE
+                            }
+
+                            linearLayout{
+                                playButton = imageButton{
+                                    imageResource = R.drawable.ic_play_circle_outline_black_24dp
+                                    onClick {
+                                        playOrPause()
+                                    }
+                                }.lparams(0, wrapContent,0.5f)
+
+                            }.lparams(matchParent, wrapContent){
+                                topMargin = dip(5)
+                            }
+
+
+
+                        }.lparams(matchParent, wrapContent){
+                            alignParentBottom()
+                        }
+                    }
+
+                }
+
+                fun playRandom(){
+                    Collections.shuffle(songs)
+                    val song = songs[0]
+                    mediaPlayer?.reset()
+                    mediaPlayer = MediaPlayer.create(ctx,song.uri)
+                    mediaPlayer?.setOnCompletionListener {
+                        playRandom()
+                    }
+                    albumArt?.imageURI = song.albumArt
+                    songTitle?.text = song.title
+                    songArtist?.text = song.artist
+                    mediaPlayer?.start()
+                    playButton?.imageResource = R.drawable.ic_play_circle_outline_black_24dp
+                }
+
+                fun playOrPause(){
+                    var songPlaying:Boolean? = mediaPlayer?.isPlaying
+
+                    if(songPlaying == true){
+                        mediaPlayer?.pause()
+                        playButton?.imageResource = R.drawable.ic_play_circle_outline_black_24dp
+                    }
+                    else{
+                        mediaPlayer?.start()
+                        playButton?.imageResource = R.drawable.ic_pause_circle_outline_black_24dp
+                    }
+                }
+            }
+
+            playerUI.setContentView(this@MainActivity)
+            playerUI.playRandom()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -85,5 +176,10 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDestroy() {
+        mediaPlayer?.release()
+        super.onDestroy()
     }
 }
