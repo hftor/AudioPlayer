10f6fc73aca6c39f800d473f7c3051414a3318f3
Haftor <haftor.g@gmail.com>
2018-11-05 17:02:00 +0000

    Adds access to external storage.

--- app/src/main/java/com/test/audioplayer/MainActivity.kt
+++ app/src/main/java/com/test/audioplayer/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.test.audioplayer
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.longToast
+import java.io.File
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    var songTitle: TextView? = null
+    var songArtist: TextView? = null
+    var albumArt: ImageView? = null
+
+    var playButton: ImageButton? = null;
+
+    var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +38,44 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 0)
+        }
+        else
+        {
+            createPlayer()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            createPlayer()
+        }
+        else
+        {
+            longToast("Permission not granterd")
+        }
+    }
+
+    private fun createPlayer()
+    {
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+
         return true
     }
+
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
